5c45ef5b539b06242d3e263aa8218f28f511efc1
Chawki <chawkimbarki500@gmail.com>
2023-05-25 10:36:55 +0100

    Update stringLength and it's test

--- modules/functions/stringLength.js
+++ modules/functions/stringLength.js
@@ -1,5 +1,9 @@
 function stringLength(string) {
-    return string.length
+    const length = string.length;
+    if (length === 0) {
+        throw new Error("String shouldn't be empty");
+    } 
+    return length;
 }
 
 module.exports = stringLength
--- tests/stringLength.test.js
+++ tests/stringLength.test.js
@@ -1,18 +1,11 @@
 const stringLength = require('../modules/functions/stringLength.js')
 
-it("Check length(1)", () => {
-    testString = "test"
+it("Check length", () => {
+    const testString = "test"
     expect(stringLength(testString)).toBe(4)
 })
-it("Check length(2)", () => {
-    testString = ""
-    expect(stringLength(testString)).toBe(0)
-})
-it("Check length(3)", () => {
-    testString = "te"
-    expect(stringLength(testString)).toBe(2)
-})
-it("Check length(4)", () => {
-    testString = "testtest "
-    expect(stringLength(testString)).toBe(9)
+
+it("String shouldn't be empty", () => {
+    const testString = ""
+    expect(stringLength(testString)).toBeGreaterThan(0)
 })
