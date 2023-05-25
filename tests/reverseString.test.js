bbd61cc95d5fcbaca50ae7362308b826467d9e52
Chawki <chawkimbarki500@gmail.com>
2023-05-25 10:38:52 +0100

    Create reverseString.test.js

---
+++ tests/reverseString.test.js
@@ -0,0 +1,6 @@
+const reverseString = require('../modules/functions/reverseString.js')
+
+it("string should be reversed", () => {
+    const testString = "test"
+    expect(reverseString(testString)).toEqual('tet')
+})
